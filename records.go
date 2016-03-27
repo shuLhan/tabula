3b0bede153ab898dbc3daa887e2edfbefa16c884
Mhd Sulhan <ms@kilabit.info>
2016-03-28 02:07:34 +0700

    records: add function to count record by value.

--- records.go
+++ records.go
@@ -9,14 +9,39 @@ Records define slice of pointer to Record.
 */
 type Records []*Record
 
-/*
-SortByIndex sort record in column by index.
-*/
-func (recs Records) SortByIndex(sortedIdx []int) *Records {
-	sorted := make(Records, len(recs))
+//
+// SortByIndex will sort the records using slice of index `sortedIDx` and
+// return it.
+//
+func (recs *Records) SortByIndex(sortedIdx []int) *Records {
+	sorted := make(Records, len(*recs))
 
-	for i := range sortedIdx {
-		sorted[i] = recs[sortedIdx[i]]
+	for x, v := range sortedIdx {
+		sorted[x] = (*recs)[v]
 	}
 	return &sorted
+}
+
+//
+// CountWhere return number of record where its value is equal to `v` type and
+// value.
+//
+func (recs *Records) CountWhere(v interface{}) (c int) {
+	for _, r := range *recs {
+		if r.IsEqual(v) {
+			c++
+		}
+	}
+	return
+}
+
+//
+// CountsWhere will return count of each value in slice `sv`.
+//
+func (recs *Records) CountsWhere(vs []interface{}) (counts []int) {
+	for _, v := range vs {
+		c := recs.CountWhere(v)
+		counts = append(counts, c)
+	}
+	return
 }
